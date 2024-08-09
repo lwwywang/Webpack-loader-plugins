3d38a436a509c1a19e69003728efdcec22a3f2f8
lwwywang <14119472+lwwywang@user.noreply.gitee.com>
2024-08-09 12:57:55 +0800

    fix version & delete comment

--- loaders/markdown-loader.js
+++ loaders/markdown-loader.js
@@ -1,8 +1,5 @@
-//获取参数
 const { getOptions } = require('loader-utils')
-//校验用户传递参数是否正确
 const validateOptions = require('schema-utils')
-//引入markdown-it
 const MarkdownIt  = require('markdown-it')
 
 
@@ -19,7 +16,6 @@ const schema = {
     langPrefix: {
     	type: 'string'
     },
-    //markdown-it的配置
     linkify: {
     	type: 'boolean'
     }
@@ -29,9 +25,7 @@ const schema = {
 module.exports =  function(source) {
 
   const options = getOptions(this)
-  //合并用户传递的参数和默认参数
   const md = MarkdownIt(options)
-//用来验证用户传递的参数是否正确
   validateOptions(schema, options)
 
   return md.render(source)
